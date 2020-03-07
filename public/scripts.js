79f39fa31d883abdc9133f3a4cfe9ba46d6db184
primobec <lucas.franca.94@hotmail.com>
2020-03-07 19:20:21 -0300

    Incluída função do menu ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,40 +1,8 @@
-/* MODAL
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
+const currentPage = location.pathname;
+const menuItems = document.querySelectorAll("header .links a");
 
-for (let card of cards){
-  card.addEventListener("click", function(){
-    const videoId = card.getAttribute("id");
-    modalOverlay.classList.add('active');
-    
-    modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
-  })
+for (item of menuItems) {
+  if (currentPage.includes(item.getAttribute("href"))){
+    item.classList.add("active")
+  }
 }
-
-document.querySelector('.close-modal').addEventListener("click",function(){
-  modalOverlay.classList.remove('active');
-  
-  modalOverlay.querySelector("iframe").src = "";
-});
-
-
-const modal = document.querySelector('.modal');
-modal.addEventListener("dblclick", function(){
-  if(modal.classList.contains('maximize')){
-    modal.classList.remove('maximize');
-  } else{
-    modal.classList.add('maximize');
-  } 
-});
-*/
-
-/* Video Page*/
-const cards = document.querySelectorAll('.card');
-
-for (let card of cards){
-  card.addEventListener("click", function(){
-    const videoId = card.getAttribute("id");
-    
-    window.location.href = `/video?id=${videoId}`
-  })
-} 
